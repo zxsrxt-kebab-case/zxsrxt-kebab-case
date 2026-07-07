03e56e961e52086eeedf83bdfc83affb8b0dfabc
zxsrxt <107710425+zxsrxt-kebab-case@users.noreply.github.com>
2026-07-07 14:16:53 +0500

    Update digital.js

--- digital.js
+++ digital.js
@@ -1,11 +1,30 @@
-let body = JSON.parse($response.body);
+try {
+  let body = JSON.parse($response.body);
 
-let docs = body?.raw_data?.digitalProfile?.documents;
-if (docs?.RF_PASSPORT?.info) {
-  for (let p of docs.RF_PASSPORT.info) {
-    p.series = "1234";
-    p.number = "123456";
+  let docs = body?.raw_data?.digitalProfile?.documents;
+  if (docs?.RF_PASSPORT) {
+    let pp = docs.RF_PASSPORT;
+
+    if (pp.info) {
+      for (let p of pp.info) {
+        p.series = "1234";
+        p.number = "123456";
+      }
+    }
+
+    if (pp.infoRaw) {
+      let raw = JSON.parse(pp.infoRaw);
+      if (Array.isArray(raw)) {
+        for (let p of raw) {
+          p.series = "1234";
+          p.number = "123456";
+        }
+      }
+      pp.infoRaw = JSON.stringify(raw);
+    }
   }
-}
 
-$done({body: JSON.stringify(body)});
+  $done({body: JSON.stringify(body)});
+} catch (e) {
+  $done({});
+}
